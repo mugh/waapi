ee6805edd9b3ecbf3faf43b9303a2dcc815aea21
Mughni Yumashar <mughnimail@gmail.com>
2024-07-24 08:37:57 +0700

    Add files via upload

---
+++ PHP TEST SCRIPT/sending.php
@@ -0,0 +1,19 @@
+<?php
+include 'waapi.php';
+
+$id="mugh";
+$url="http://localhost:3000";
+$apikey = getapi($url, $id);
+//echo $apikey;
+
+$number="6282129080941";
+$textcontent="aku disini untukmu,
+coba kamu cari tahu";
+$caption="attachment buat kamu";
+$imageurl = "http://localhost/mygage2024/files/img.jpg";
+$fileurl = "http://localhost/mygage2024/files/900747. Termometer. 2023. 10.13-rev1.pdf";
+
+//text($url, $id, $number, $textcontent, $apikey);
+//image($url, $id, $number, $caption, $imageurl, $apikey);
+//sendfile($url, $id, $number, $caption, $fileurl, $apikey);
+
